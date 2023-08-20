5ea0a897c364f89c7126a0e92a8c92cb1e485214
leonardogonfiantini <lp.gonfiantini@gmail.com>
2023-08-20 12:31:55 +0200

    New function upgradeSlot for upgrading a slot automatically, added also the testfunction to test the functions

--- app/core.js
+++ app/core.js
@@ -1,9 +1,13 @@
 import { humanType, humanClick, humanRandomMouseMove, randomDelay } from './human.js';
-import { getVillagesList, getVillageBuildingsInfo, getVillageResourcesInfo } from './utils.js';
+import { getVillagesList, getVillageBuildingsInfo, getVillageResourcesInfo, checkVillageBuildingList } from './utils.js';
+
+export async function testFunction(page) {
+    await checkVillageBuildingList(page);
+}
 
 export async function login(page, username, password) {
 
-    await page.goto('https://ts4.x1.europe.travian.com/logout');
+    await page.goto('https://ts4.x1.europe.travian.com/');
 
     const usernameInput = await page.locator('input[name="name"]');
     const passwordInput = await page.locator('input[name="password"]');
@@ -38,8 +42,35 @@ export async function getVillagesInfo(page) {
     await humanRandomMouseMove(page);
     await randomDelay(page);
 
-    console.log(accountInfo)
+
 
     return accountInfo;
     
+}
+
+export async function upgradeSlot(page, slot_url) {
+
+    const availableSlots = await checkVillageBuildingList(page);
+    if (!availableSlots) {
+        console.log('No available slots');
+        return false;
+    }
+
+    await page.goto(`https://ts4.x1.europe.travian.com${slot_url}`);
+    await humanRandomMouseMove(page);
+    await randomDelay(page);
+
+    const buttonUpgradeContainer = await page.waitForSelector('div.section1');
+    const buttonUpgrade = await buttonUpgradeContainer.$('button');
+
+    const buttonClass = await page.evaluate(el => el.getAttribute('class'), buttonUpgrade)
+    if (buttonClass.includes('gold')) {
+        return false;
+    }
+
+    await humanRandomMouseMove(page);
+    await humanClick(buttonUpgrade, page);
+
+    return true;
+
 }
