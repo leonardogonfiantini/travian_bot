28ed26c125c6fc46d96afd9e7848151f058a02d5
leonardogonfiantini <lp.gonfiantini@gmail.com>
2023-08-20 12:32:53 +0200

    An example of auto builder with level increase

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import dotenv from 'dotenv';
 
-import { login, getVillagesInfo } from './core.js'
+import { login, getVillagesInfo, testFunction, upgradeSlot } from './core.js'
 
 // Register the Stealth plugin
 puppeteer.use(StealthPlugin());
@@ -14,15 +14,43 @@ puppeteer.use(StealthPlugin());
 
     const page = await browser.newPage();
 
+
     dotenv.config();
     const username = process.env.TRAVIAN_USERNAME;
     const password = process.env.TRAVIAN_PASSWORD;
 
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
     await login(page, username, password);
 
-    await getVillagesInfo(page);
+    while(true) {
 
-    await new Promise(resolve => setTimeout(resolve, 10000));
+        //upgrade all the village resources level by level
+        const villagesInfo = await getVillagesInfo(page);
+
+        let min_level = 16; 
+
+        for (const villageInfo of villagesInfo) {  
+            for (const resource of villageInfo.resources) {
+                if (resource.level < min_level) {
+                    min_level = resource.level;
+                }
+            }
+        }
+
+        for (const villageInfo of villagesInfo) {
+            for (const resource of villageInfo.resources) {
+                if (resource.level == min_level && resource.status == 'normal') {
+                    const status = await upgradeSlot(page, resource.href);
+                    if (!status) break;
+                }
+            }
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 20));
+
+    }
+
 
     // Close the browser
     await browser.close();
